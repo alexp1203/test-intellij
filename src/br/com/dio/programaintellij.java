73db1721d09c080fcf06523a76ec5b4db48d6d76
alexp1203 <alexpinheiro1203@gmail.com>
2023-02-12 22:33:28 -0300

    first commit intellij

--- src/br/com/dio/programaintellij.java
+++ src/br/com/dio/programaintellij.java
@@ -4,9 +4,9 @@ import br.com.dio.model.Gato;
 
 public class programaintellij {
     public static void main(String[] args) {
-        /*int a = 5;
+        int a = 5;
         int b = 3;
-        System.out.println("Hello World!! " + (a+b));*/
+        System.out.println("Hello World!! " + (a+b));
         Gato gato = new Gato();
 
         System.out.println(gato);
